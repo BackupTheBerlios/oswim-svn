d652f0c90bf720b68b861676184623505b4aa62b
klevenzs <klevenzs@df8ad11c-7278-0410-9221-fcdb2fed2018>
2010-01-01 17:43:50 +0000

    openswim : sandbox : hello world osgi code added

---
+++ trunk/sandbox/sandbox.helloworld.osgi/src/main/java/org/openswim/sandbox/helloworld/osgi/HelloWorldOsgi.java
@@ -0,0 +1,17 @@
+package org.openswim.sandbox.helloworld.osgi;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+public class HelloWorldOsgi {
+
+	private static Log log = LogFactory.getLog(HelloWorldOsgi.class);
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		HelloWorldOsgi.log.info("OpenSWIM:Sandbox:HelloWorldOsgi !!!");
+	}
+
+}
