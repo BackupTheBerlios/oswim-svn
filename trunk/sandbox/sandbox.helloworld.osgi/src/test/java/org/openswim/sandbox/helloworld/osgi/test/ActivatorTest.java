a1a88e74eb5c1294aa512ce00f1c0928adbaf79f
klevenzs <klevenzs@df8ad11c-7278-0410-9221-fcdb2fed2018>
2010-02-14 09:19:27 +0000

    activator test added

---
+++ trunk/sandbox/sandbox.helloworld.osgi/src/test/java/org/openswim/sandbox/helloworld/osgi/test/ActivatorTest.java
@@ -0,0 +1,32 @@
+package org.openswim.sandbox.helloworld.osgi.test;
+
+import static org.easymock.EasyMock.createNiceMock;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.openswim.sandbox.helloworld.osgi.Activator;
+import org.osgi.framework.BundleContext;
+
+public class ActivatorTest {
+
+	private Activator activator = null;
+	private BundleContext ctx = null;
+
+	@Before
+	public void setup() {
+		this.activator = new Activator();
+		this.ctx = createNiceMock(BundleContext.class);
+
+	}
+
+	@Test
+	public void startActivation() throws Exception {
+		this.activator.start(this.ctx);
+	}
+
+	@Test
+	public void stopActivation() throws Exception {
+		this.activator.start(this.ctx);
+	}
+
+}
